41dc2d1e50e4fc69ac52cb3274e5dc94c57be669
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-10-09 10:36:37 +0100

    Update crown-momorial.cpp

--- crown-momorial.cpp
+++ crown-momorial.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 void crown(int length, int height) {
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < length; j++) {
+      if (i == 0) {
+        
+        if (j == 0 || j == height || j == length - 1) {
+                    cout << "*";
+                }
+                else {
+                    cout << " ";
+                }
+      
+      }
+      
+      else if (i == height - 1) {
+                cout << "-";
+            }
+    }
+  }
 }
 
 int main()
